c1410aaf06c69cba7a1deda1306a2722520b8698
ErenBerkSari <109284000+ErenBerkSari@users.noreply.github.com>
2022-07-14 14:05:15 +0300

    Add files via upload

---
+++ Create_Komutu.sql
@@ -0,0 +1,6 @@
+--Create komutu tablo olýuþturmak için kullanýlan komut.
+create table Kitaplar2(KitapID int not null,
+KitapAd nvarchar(50) not null,
+Yazar nvarchar(50) not null,
+Yayinevi nvarchar(50) not null,
+Fiyat smallmoney not null)
